e776c6fe64a69f09187b27bb81774af3fb4f3466
aliamer229 <79536333+aliamer229@users.noreply.github.com>
2026-08-25 06:55:13 +0300

    Add secure in-app updates and internal legal notices

--- mobile/android/app/src/main/java/iq/levo/studio/MainActivity.java
+++ mobile/android/app/src/main/java/iq/levo/studio/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(LevoPrinterPlugin.class);
+        registerPlugin(LevoUpdaterPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
